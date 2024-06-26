732b6d4d92d7ac572e6bdb659210e9743ca5a293
Bernard McGeever <bernard.mcgeever@give-it-a-bash.com>
2024-06-26 20:23:27 +0100

    created and styled home and contact page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavagationComponent } from './navagation/navagation.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavagationComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
